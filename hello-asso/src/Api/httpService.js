09c8885297a537edda6a46c0232f83e2ca409e77
yoan <yoan.borne@epitech.eu>
2022-08-18 17:56:25 +0200

    instance axios creer (voir dossier api)

--- hello-asso/src/Api/httpService.js
+++ hello-asso/src/Api/httpService.js
@@ -1,8 +1,24 @@
 import axios from  'axios';
+// ajout intercepteur de request
+axios.interceptors.request.use(
+    function (config) {
+        // Faire  avant que la demande ne soit envoyée
+        config.headers.Authorization = `Bearer ${your_token}`;
+        // ou config.headers.common['Authorization'] = `Bearer ${your_token}` ;
+        config.baseURL = '';//mettre url test api
 
-const httpService = axios.create({
-    BaseURL: '',
-    timeout: 1799,
-    
-})
-export default httpService;
+        return config;
+    },
+    function (error) {
+        // les erreurs
+        return Promise.reject(error);
+    }
+);
+
+export default {
+    get: axios.get,
+    post: axios.post,
+    put: axios.put,
+    delete: axios.delete,
+    patch: axios.patch
+};
--- hello-asso/src/Components/Menu.js
+++ hello-asso/src/Components/Menu.js
@@ -1,9 +1,10 @@
-
+import Home from '../Views/Home';
 const menuItems = [
 {
     title: "Home",
-    url: "/",
+    url: Home,
     cName: "nav-links",
+
     
 },
 {
